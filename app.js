f1aaa0a3a8ffe8705405e5c98de41778842ad72b
karthikjain149 <karthikrmjain149@gmail.com>
2023-05-02 02:55:40 +0530

    javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+// console.log("Script")
+
+//input
+var username = prompt("Give me a username")
+//processing
+var welcomeMessage = "This Script Works! " + username
+//output
+alert(welcomeMessage)
